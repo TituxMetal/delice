6d7e836e475f4a9f3e7a66f7a9deb2b8f85ea41b
Titux Metal <github@lgdweb.fr>
2023-07-07 05:18:13 +0200

    Create post-install.sh
    Installs just the minimum packages utilities such as git, htop, netselect-apt and Ly display manager.

---
+++ post-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install -y vim htop git \
+  build-essential libpam0g-dev libxcb-xkb-dev \
+  netselect-apt lynis duf systemd-zram-generator
+
+git clone --recurse-submodules https://github.com/fairyglade/ly && cd ly
+make && make run && make install installsystemd
+
+systemctl enable ly.service && systemctl disable getty@tty2.service
