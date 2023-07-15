bd39bf8951cc996474738cd2c3af9acb34576026
Titux Metal <github@lgdweb.fr>
2023-07-15 07:47:23 +0200

    Update desktop-environment.sh
    Fix some issues with package names and re-organize all things.

--- desktop-environment.sh
+++ desktop-environment.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-apt install -y task-xfce-desktop spice-vdagent xserver-xorg-video-qxl xserver-xorg-video-vesa accountsservice slick-greeter \
-  fonts-terminus papirus-icon-theme gtk-update-icon-cache arc-theme \
-  firefox-esr firefox-esr-l10n-fr fonts-lmodern \
-  libreoffice-gtk3 libreoffice-l10n-fr
+# Run these commands with root user
+
+# Video drivers: please uncomment Intel or Nvidia if necessary
+
+# Generics
+apt install -y spice-vdagent xserver-xorg-video-qxl xserver-xorg-video-vesa
+
+# Intel Graphics
+# apt install -y libva2 intel-media-va-driver xserver-xorg-video-intel
+
+# Nvidia
+# apt install -y xserver-xorg-video-nouveau nvidia-vaapi-driver
+
+# Xfce desktop
+apt install -y task-xfce-desktop accountsservice slick-greeter
 
 # Pre fill default user in lightdm
 
@@ -16,68 +27,45 @@ greeter-show-manual-login=true
 EOF
 ```
 
+# X server utilities
+apt install -y xbindkeys xdg-utils xdg-user-dirs xdg-user-dirs-gtk xbindkeys xcompmgr numlockx tumbler xbacklight xvkbd xinput
 
+# Themes and fonts
+apt install -y lxappearance fonts-recommended fonts-ubuntu fonts-font-awesome fonts-lmodern fonts-terminus \
+  papirus-icon-theme gtk-update-icon-cache arc-theme adwaita-qt qt5ct
 
+# Web Browser
+apt install -y torbrowser-launcher firefox-esr firefox-esr-l10n-fr
 
+# Office Tools
+apt install -y gpa gnupg ghostscript libreoffice-gtk3 libreoffice-l10n-fr libreoffice-help-fr galculator l3afpad ghostwriter
 
-# Video drivers: please uncomment Intel or Nvidia if necessary
-
-# Generics
-apt install -y mesa-va-drivers mesa-utils mesa-vdpau-drivers mesa-vulkan-drivers libvdpau-va-gl1 xserver-xorg-video-vesa
-
-# Intel Graphics
-# apt install -y libva2 intel-media-va-driver xserver-xorg-video-intel
-
-# Virt Manager
-apt install -y spice-vdagent xserver-xorg-video-qxl
-
-# Nvidia
-# apt install -y xserver-xorg-video-nouveau nvidia-vaapi-driver
+# Other Utils
+apt install -y neofetch alacritty rofi pcmanfm libfm-tools libusbmuxd-tools feh dex xarchiver gparted gphoto2 sshfs nfs-common fuseiso \
+  file-roller timeshift gvfs gvfs-backends gvfs-fuse dunst libnotify-bin figlet qimgv redshift cpu-x cryptsetup policykit-1-gnome \
+  pavucontrol alsa-utils pulseaudio ffmpeg ffmpegthumbnailer
 
 # Multimedia: please uncomment what you need
+# Gstreamer 
+apt install -y gstreamer1.0-x gstreamer1.0-libav
 
 # Players
-apt install -y vlc quodlibet shotwell evince mpd playerctl
+apt install -y vlc quodlibet shotwell evince mpd playerctl x265 x264 libdvd-pkg libdvdread8 libdvdnav4 libcdio19
+
+# Download libdvdcss2 source files
+sudo dpkg-reconfigure libdvd-pkg
 
 # Editors
-# apt install -y gimp inkscape scour vokoscreen-ng
+apt install -y gimp inkscape scour
 
 # Burn
-# apt install -y libdvd-pkg brasero dvdauthor dvdbackup dvd+rw-tools libdvdread8 libdvdnav4 libcdio19 libisoburn1
+# apt install -y brasero dvdauthor dvdbackup dvd+rw-tools libisoburn1
 
 # Rip/Encode
-# apt install -y asunder handbrake handbrake-cli
-
-# Gstreamer 
-apt install -y gstreamer1.0-vaapi gstreamer1.0-alsa gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good gstreamer1.0-plugins-base \
-  gstreamer1.0-plugins-bad gstreamer1.0-nice gstreamer1.0-gtk3 gstreamer1.0-gl gstreamer1.0-fdkaac gstreamer1.0-x gstreamer1.0-libav
-
-# Audio/Video Codec
-apt install -y libfdk-aac2 fdkaac libvorbis0a libdca0 libwebpmux3 libwebpdemux2 libwebp7 libtheora0 libvpx7 libxvidcore4 x265 x264 \
-ogmtools ffmpeg ffmpegthumbnailer faac libfaad2 lame flac wavpack pavucontrol alsa-utils pulseaudio
+# apt install -y asunder handbrake handbrake-cli lame ogmtools faac flac x265 x264
 
 # Bluetooth
 # apt install -y bluez bluez-firmware bluez-tools pulseaudio-module-bluetooth blueman
-
-# Themes and fonts
-apt install -y lxappearance fonts-recommended fonts-font-awesome fonts-terminus papirus-icon-theme gtk-update-icon-cache arc-theme
-
-# Utils
-apt install -y neofetch alacritty rofi pcmanfm feh galculator dex xarchiver gparted gvfs gphoto2 gmtp nfs-common libgsf-1-114 \
-  sshfs fuseiso file-roller policykit-1-gnome network-manager timeshift gvfs-backends gvfs-fuse dunst libnotify-bin figlet \
-  qimgv l3afpad redshift cpu-x ghostwriter
-
-# Web Browser
-apt install -y torbrowser-launcher firefox-esr firefox-esr-l10n-fr fonts-lmodern libcanberra0 libcanberra-gtk0
-
-# Office
-apt install -y gpa gnupg ghostscript libreoffice-gtk3 libreoffice-l10n-fr libreoffice-help-fr
-
-# X server
-apt install -y xbindkeys xdg-utils xdg-user-dirs xdg-user-dirs-gtk xbindkeys xcompmgr numlockx tumbler xbacklight xvkbd xinput
-
-# Xfce Desktop
-# xfce4-mpc-plugin xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin adwaita-qt qt5ct
 
 # Re-enable Ly as display manager and remove lightdm
 # systemctl disable display-manager.service && apt remove --purge -y lightdm lightdm-gtk-greeter && apt -y autoremove && systemctl enable ly
