bbf3bf366d5aba080f31f35bf9ce28f83417198f
Titux Metal <github@lgdweb.fr>
2023-07-16 11:29:49 +0200

    Update script; fix running in tty not working.

--- desktop-environment.sh
+++ desktop-environment.sh
@@ -1,75 +1,98 @@
 #!/bin/bash
 
-# Run these commands with root user
+# Run these commands with regular user
+
+printMessage() {
+  message=$1
+  tput setaf 2
+  echo "-------------------------------------------"
+  echo "$message"
+  echo "-------------------------------------------"
+  tput sgr0
+}
+
+# Helper function to handle errors
+handleError() {
+  clear
+  set -uo pipefail
+  trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+}
+
+main() {
+  handleError
 
 # Video drivers: please uncomment Intel or Nvidia if necessary
 
 # Generics
-apt install -y spice-vdagent xserver-xorg-video-qxl xserver-xorg-video-vesa
+  sudo apt install -y spice-vdagent xserver-xorg-video-qxl xserver-xorg-video-vesa
 
-# Intel Graphics
-# apt install -y libva2 intel-media-va-driver xserver-xorg-video-intel
+  # Intel Graphics
+  # sudo apt install -y libva2 intel-media-va-driver xserver-xorg-video-intel
 
-# Nvidia
-# apt install -y xserver-xorg-video-nouveau nvidia-vaapi-driver
+  # Nvidia
+  # sudo apt install -y xserver-xorg-video-nouveau nvidia-vaapi-driver
 
-# Xfce desktop
-apt install -y task-xfce-desktop accountsservice slick-greeter
+  # Xfce desktop
+  sudo apt install -y task-xfce-desktop accountsservice slick-greeter
 
-# Pre fill default user in lightdm
+  # Pre fill default user in lightdm
 
-``` bash
-mkdir -p /etc/lightdm/lightdm.conf.d && touch /etc/lightdm/lightdm.conf.d/auto-user.conf \
-  && cat > /etc/lightdm/lightdm.conf.d/auto-user.conf<< EOF
+  sudo mkdir -p /etc/lightdm/lightdm.conf.d && sudo touch /etc/lightdm/lightdm.conf.d/auto-user.conf && sudo cat > /etc/lightdm/lightdm.conf.d/auto-user.conf<< EOF
 [SeatDefaults]
-greeter-hide-users=false
-greeter-show-manual-login=true
+  greeter-hide-users=false
+  greeter-show-manual-login=true
 EOF
-```
 
-# X server utilities
-apt install -y xbindkeys xdg-utils xdg-user-dirs xdg-user-dirs-gtk xbindkeys xcompmgr numlockx tumbler xbacklight xvkbd xinput
 
-# Themes and fonts
-apt install -y lxappearance fonts-recommended fonts-ubuntu fonts-font-awesome fonts-lmodern fonts-terminus \
-  papirus-icon-theme gtk-update-icon-cache arc-theme adwaita-qt qt5-style-kvantum
+  # X server utilities
+  sudo apt install -y xbindkeys xdg-utils xdg-user-dirs xdg-user-dirs-gtk xbindkeys xcompmgr numlockx tumbler xbacklight xvkbd xinput
 
-# Web Browser
-apt install -y torbrowser-launcher firefox-esr firefox-esr-l10n-fr
+  # Themes and fonts
+  sudo apt install -y lxappearance fonts-recommended fonts-ubuntu fonts-font-awesome fonts-lmodern fonts-terminus \
+    papirus-icon-theme gtk-update-icon-cache arc-theme adwaita-qt qt5-style-kvantum
 
-# Office Tools
-apt install -y gpa gnupg ghostscript libreoffice-gtk3 libreoffice-l10n-fr libreoffice-help-fr \
-  galculator l3afpad ghostwriter libtext-multimarkdown-perl cmark pandoc
+  # Web Browser
+  sudo apt install -y torbrowser-launcher firefox-esr firefox-esr-l10n-fr
 
-# Other Utils
-apt install -y neofetch alacritty rofi pcmanfm libfm-tools libusbmuxd-tools feh dex xarchiver gparted gphoto2 sshfs nfs-common fuseiso \
-  file-roller timeshift gvfs gvfs-backends gvfs-fuse dunst libnotify-bin figlet qimgv redshift cpu-x cryptsetup policykit-1-gnome \
-  pavucontrol alsa-utils pulseaudio ffmpeg ffmpegthumbnailer
+  # Office Tools
+  sudo apt install -y gpa gnupg ghostscript libreoffice-gtk3 libreoffice-l10n-fr libreoffice-help-fr \
+    galculator l3afpad ghostwriter libtext-multimarkdown-perl cmark pandoc
 
-# Multimedia: please uncomment what you need
-# Gstreamer 
-apt install -y gstreamer1.0-x gstreamer1.0-libav
+  # Other Utils
+  sudo apt install -y neofetch alacritty rofi pcmanfm libfm-tools libusbmuxd-tools feh dex xarchiver gparted gphoto2 sshfs nfs-common fuseiso \
+    file-roller timeshift gvfs gvfs-backends gvfs-fuse dunst libnotify-bin figlet qimgv redshift cpu-x cryptsetup policykit-1-gnome \
+    pavucontrol alsa-utils pulseaudio ffmpeg ffmpegthumbnailer
 
-# Players
-apt install -y vlc quodlibet shotwell evince mpd playerctl x265 x264 libdvd-pkg libdvdread8 libdvdnav4 libcdio19
+  # Multimedia: please uncomment what you need
+  # Gstreamer 
+  sudo apt install -y gstreamer1.0-x gstreamer1.0-libav
 
-# Download libdvdcss2 source files
-sudo dpkg-reconfigure libdvd-pkg
+  # Players
+  sudo apt install -y vlc quodlibet shotwell evince mpd playerctl x265 x264 libdvd-pkg libdvdread8 libdvdnav4 libcdio19
 
-# Editors
-apt install -y gimp inkscape scour
+  # Download libdvdcss2 source files
+  sudo dpkg-reconfigure libdvd-pkg
 
-# Burn
-# apt install -y brasero dvdauthor dvdbackup dvd+rw-tools libisoburn1
+  # Editors
+  sudo apt install -y gimp inkscape scour
 
-# Rip/Encode
-# apt install -y asunder handbrake handbrake-cli lame ogmtools faac flac x265 x264
+  # Burn
+  # sudo apt install -y brasero dvdauthor dvdbackup dvd+rw-tools libisoburn1
 
-# Bluetooth
-# apt install -y bluez bluez-firmware bluez-tools pulseaudio-module-bluetooth blueman
+  # Rip/Encode
+  # sudo apt install -y asunder handbrake handbrake-cli lame ogmtools faac flac x265 x264
 
-# Re-enable Ly as display manager and remove lightdm
-# systemctl disable display-manager.service && apt remove --purge -y lightdm lightdm-gtk-greeter && apt -y autoremove && systemctl enable ly
+  # Bluetooth
+  # sudo apt install -y bluez bluez-firmware bluez-tools pulseaudio-module-bluetooth blueman
 
-# Copy all config files and directories in users .config
-cp -rv .config/* ~/.config/
+  # Re-enable Ly as display manager and remove lightdm
+  # systemctl disable display-manager.service && sudo apt remove --purge -y lightdm lightdm-gtk-greeter && sudo apt -y autoremove && systemctl enable ly
+
+  # Copy all config files and directories in users .config
+  cp -rv .config/* ~/.config/
+
+}
+
+time main
+
+exit 0
--- post-install.sh
+++ post-install.sh
@@ -1,37 +1,64 @@
 #!/bin/bash
 
-# Make a backup of sources.list file
-sudo cp -v /etc/apt/sources.list /etc/apt/sources.list.bak
+# Run these commands with regular user
 
-# Add the new sources.list file
-sudo cp -v sources.list /etc/apt/sources.list
+printMessage() {
+  message=$1
+  tput setaf 2
+  echo "-------------------------------------------"
+  echo "$message"
+  echo "-------------------------------------------"
+  tput sgr0
+}
 
-# Make a full system upgrade
-sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
+# Helper function to handle errors
+handleError() {
+  clear
+  set -uo pipefail
+  trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+}
 
-# Run this script as regular user
-sudo apt install -y vim htop git bash-completion rsync curl wget chrony modemmanager ufw iwd dnsutils libnss-mdns avahi-daemon \
-  build-essential libpam0g-dev libxcb-xkb-dev fdisk mtools xfsprogs dosfstools zip unzip unrar p7zip-full f2fs-tools exfatprogs \
-  gpart udftools netselect-apt lynis duf systemd-zram-generator
+main() {
+  handleError
 
-sudo systemctl enable chrony ssh avahi-daemon fstrim.timer ufw iwd ModemManager
+  # Make a backup of sources.list file
+  sudo cp -v /etc/apt/sources.list /etc/apt/sources.list.bak
 
-# Setup time
-sudo timedatectl set-timezone Europe/Paris --adjust-system-clock
-sudo timedatectl set-ntp yes
+  # Add the new sources.list file
+  sudo cp -v sources.list /etc/apt/sources.list
 
-# Setup Ufw firewall
-sudo ufw default deny incoming && ufw allow ssh && ufw default allow outgoing
+  # Make a full system upgrade
+  sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
 
-# Install and setup Ly display manager
-# git clone --recurse-submodules https://github.com/fairyglade/ly && cd ly
-# make && make install installsystemd
+  # Run this script as regular user
+  sudo apt install -y vim htop git bash-completion rsync curl wget chrony modemmanager ufw iwd dnsutils libnss-mdns avahi-daemon \
+    build-essential libpam0g-dev libxcb-xkb-dev fdisk mtools xfsprogs dosfstools zip unzip unrar p7zip-full f2fs-tools exfatprogs \
+    gpart udftools netselect-apt lynis duf systemd-zram-generator
 
-# systemctl enable ly.service && systemctl disable getty@tty2.service
+  sudo systemctl enable chrony ssh avahi-daemon fstrim.timer ufw iwd ModemManager
 
-# Add dot files to regular user
-cp -v dotfiles/.* $HOME/
-ln -s .profile .xsessionrc
+  # Setup time
+  sudo timedatectl set-timezone Europe/Paris --adjust-system-clock
+  sudo timedatectl set-ntp yes
 
-# Add dot files to root user
-sudo -s cp -v dotfiles/root/.* /root/
+  # Setup Ufw firewall
+  sudo ufw default deny incoming && ufw allow ssh && ufw default allow outgoing
+
+  # Install and setup Ly display manager
+  # git clone --recurse-submodules https://github.com/fairyglade/ly && cd ly
+  # make && make install installsystemd
+
+  # systemctl enable ly.service && systemctl disable getty@tty2.service
+
+  # Add dot files to regular user
+  cp -v dotfiles/.* $HOME/
+  ln -s .profile .xsessionrc
+
+  # Add dot files to root user
+  sudo -s cp -v dotfiles/root/.* /root/
+
+}
+
+time main
+
+exit 0
