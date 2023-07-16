a30e83f25f2b08f6e6442e4341b8fe7dd6f0d191
Titux Metal <github@lgdweb.fr>
2023-07-16 04:44:02 +0200

    Update post-install.sh
    Add commands to copy dot files for root and regular user.

--- post-install.sh
+++ post-install.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-
-apt install -y vim htop git bash-completion rsync curl wget chrony modemmanager ufw iwd dnsutils libnss-mdns avahi-daemon \
+# Run this script as regular user
+sudo apt install -y vim htop git bash-completion rsync curl wget chrony modemmanager ufw iwd dnsutils libnss-mdns avahi-daemon \
   build-essential libpam0g-dev libxcb-xkb-dev fdisk mtools xfsprogs dosfstools zip unzip unrar p7zip-full f2fs-tools exfatprogs \
   gpart udftools netselect-apt lynis duf systemd-zram-generator
 
-systemctl enable chrony ssh avahi-daemon fstrim.timer ufw iwd ModemManager
+sudo systemctl enable chrony ssh avahi-daemon fstrim.timer ufw iwd ModemManager
 
 # Setup time
-timedatectl set-timezone Europe/Paris --adjust-system-clock
-timedatectl set-ntp yes
+sudo timedatectl set-timezone Europe/Paris --adjust-system-clock
+sudo timedatectl set-ntp yes
 
 # Setup Ufw firewall
-ufw default deny incoming && ufw allow ssh && ufw default allow outgoing
+sudo ufw default deny incoming && ufw allow ssh && ufw default allow outgoing
 
 # Install and setup Ly display manager
-git clone --recurse-submodules https://github.com/fairyglade/ly && cd ly
-make && make install installsystemd
+# git clone --recurse-submodules https://github.com/fairyglade/ly && cd ly
+# make && make install installsystemd
 
-systemctl enable ly.service && systemctl disable getty@tty2.service
+# systemctl enable ly.service && systemctl disable getty@tty2.service
+
+# Add dot files to regular user
+cp -v dotfiles/.* $HOME/
+ln -s .profile .xsessionrc
+
+# Add dot files to root user
+sudo -s cp -v dotfiles/root/.* /root/
